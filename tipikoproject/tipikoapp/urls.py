94fa67e66113d3915f4e8facc87803fcf8908e50
WayneOg <ogollawayne@gmail.com>
2024-10-23 11:12:03 -0700

    m

---
+++ tipikoproject/tipikoapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('events/', views.events, name='events'),
+    path('market/', views.market, name='market'),
+    path('restaurant/', views.restaurant, name='restaurant'),
+    path('shop/', views.shop, name='shop'),
+]
--- tipikoproject/tipikoapp/views.py
+++ tipikoproject/tipikoapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'index2.html')
+
+def events(request):
+    return render(request, 'events.html')
+
+
+def restaurant(request):
+    return render(request, 'restaurant.html')
+
+def market(request):
+    return render(request, 'market.html')
+
+def shop(request):
+    return render(request, 'shop.html')
